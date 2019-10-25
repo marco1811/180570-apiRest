0a1419e1bc96e467f0e68df755d08f1056204fb9
David Avila Cervantes <david.david_nauch@outlook.com>
2019-10-25 09:07:48 -0500

    Primer commit

---
+++ server/config/config.js
@@ -0,0 +1,2 @@
+// PUERTOOO
+process.env.PORT = process.env.PORT || 3000;
---
+++ server/server.js
@@ -0,0 +1,55 @@
+require('./config/config');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(bodyParser.json());
+
+app.get('/usuario', (req, res) => {
+    res.json('get usuario');
+});
+
+app.get('/usuario/:id/:nombre', (req, res) => {
+    //'params' solo escucha el url
+    let id = req.params.id;
+    let nombre = req.params.nombre;
+    res.json({
+        id,
+        nombre
+    });
+});
+
+app.post('/usuario', (req, res) => {
+    let nombre = req.body.nombre;
+    let edad = req.body.edad;
+
+    if (nombre === undefined || edad === undefined) {
+        res.status(400).json({
+            ok: 'false',
+            err: 'Favor de completar todos los campos burro'
+        });
+    } else {
+        res.json({
+            nombre,
+            edad
+        });
+    }
+});
+
+app.put('/usuario', (req, res) => {
+    res.json('put usuario');
+});
+
+app.delete('/usuario', (req, res) => {
+    res.json('delete usuario');
+});
+
+app.listen(3000, () => {
+    console.log('escuchando por el puerto 3000');
+});
+
+app.listen(process.env.PORT, () => {
+    console.log('Escuchando por el puerto ', process.env.PORT);
+});
